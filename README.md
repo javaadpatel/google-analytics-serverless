9fecd65d696e4245993ea5050b0f695df6bce5a2
javaad <javaadpatel@gmail.com>
2020-08-02 21:48:30 +0200

    Integrated Google Analytics Page View Queries

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# google-analytics-serverless
-Serverless function for retrieving google analytics page view data
+# Serverless Function for Google Analytics
+
+[![Netlify
+Status](https://api.netlify.com/api/v1/badges/9aaef7de-1e5d-4fda-bc39-faa10a68b35b/deploy-status)](https://app.netlify.com/sites/netlify-express/deploys)
+
+[![Deploy to
+Netlify](https://www.netlify.com/img/deploy/button.svg)](https://app.netlify.com/start/deploy?repository=https://github.com/javaadpatel/google-analytics-serverless)
+
+A simple serverless function, using Express.js, that retrieves Google Analytics Page View results, and can be deployed to Netlify.
+
+
+The solution is inspired by this [Netlify blog post](https://www.netlify.com/blog/2018/09/13/how-to-run-express.js-apps-with-netlify-functions/).
+
+The app is hosted at `/.netlify/functions/server`. Examples of
+how to access the Express.js endpoints:
+
+```sh
+* curl https://netlify-express.netlify.com/.netlify/functions/server/getViews
+
+* curl https://netlify-express.netlify.com/.netlify/functions/server/getViewsByPagePath?pagePath=/securing-traefik-dashboard-with-azure-ad/
+
+```
+
---
+++ express/server.js
@@ -0,0 +1,69 @@
+'use strict';
+require('dotenv').config();
+const express = require('express');
+const path = require('path');
+const serverless = require('serverless-http');
+const app = express();
+const bodyParser = require('body-parser');
+const {google} = require('googleapis');
+
+//define scopes to get metrics from google analytics
+const scopes = 'https://www.googleapis.com/auth/analytics.readonly'
+
+const privateKey = process.env.GA_PRIVATE_KEY.replace(/\\n/g, '\n')
+const jwt = new google.auth.JWT(process.env.GA_CLIENT_EMAIL, null, privateKey, scopes)
+
+const startDate = '2020-01-01'
+
+/**
+ * Retrieves unique page views from Google Analytics
+ * @param {*} pagePath the path to filter from results
+ */
+async function getUniquePageViews(pagePath) {
+  const defaults = {
+    'auth': jwt,
+    'ids': 'ga:' + process.env.GA_VIEW_ID,
+  }
+  await jwt.authorize()
+
+  const metricRequestOptions = {
+    ...defaults,
+    'start-date': startDate,
+    'end-date': 'today',
+    'metrics': 'ga:uniquePageviews',
+    'dimensions': 'ga:pagePath'
+  };
+
+  if (pagePath)
+    metricRequestOptions.filters = `ga:pagePath==${pagePath}`
+
+  const result = await google.analytics('v3').data.ga.get(metricRequestOptions)
+
+  return result.data.rows;
+}
+
+const router = express.Router();
+
+/**
+ * Get unique page views for all pages
+ */
+router.get('/getViews', async (req, res) => {
+  var data = await getUniquePageViews();
+  return res.json(data);
+});
+
+/**
+ * Get unique page views for specific page
+ */
+router.get('/getViewsByPagePath', async (req, res) => {
+  let pagePath = req.query.pagePath;
+  var data = await getUniquePageViews(pagePath);
+  return res.json(data);
+});
+
+
+app.use(bodyParser.json());
+app.use('/.netlify/functions/server', router);  // path must route to lambda
+
+module.exports = app;
+module.exports.handler = serverless(app);
